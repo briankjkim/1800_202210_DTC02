aa9fe75be6a80ef8a71e05d07552107f5fc07263
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-03-09 11:12:29 -0800

    Add firebase authentication for login, main, index pages

---
+++ scripts/firebaseAPA_DTC02.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCcK2yseB_MNDuKzKdO6vxsPN9DfQYWcWM",
+    authDomain: "team02-project.firebaseapp.com",
+    projectId: "team02-project",
+    storageBucket: "team02-project.appspot.com",
+    messagingSenderId: "558609617599",
+    appId: "1:558609617599:web:4461cdad3cafe26b68ac0a",
+    measurementId: "G-6V9W5WHK5F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
