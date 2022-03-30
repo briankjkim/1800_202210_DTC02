1acc9a99981436dbd1e89503f30f37ce27151f65
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-03-30 15:20:50 -0700

    wait_times_display_page.html is updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,139 +1,79 @@
-function sayHello() {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            // User is signed in.
-            // Do something for the user here. 
-            console.log(user.uid);
-            db.collection("users").doc(user.uid)
-                .get()
-                .then(function (doc) {
-                    var n = doc.data().name;
-                    console.log(n);
-                    $("#username").text(n);
-                    document.getElementById("username").innerText = n;
-                })
-        } else {
-            // No user is signed in.
-        }
+function writeEvents() {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var hikesRef = db.collection("events");
+
+    hikesRef.add({
+        code: "EVENT1",
+        name: "Vancouver Olympic Cauldron",
+        current_queue: "0",
+        status: "Open",
+        wait_time: "0 Minute",
+        details: "Description goes here"
+    });
+    hikesRef.add({
+        code: "EVENT2",
+        name: "Olympic Village Museum",
+        current_queue: "0",
+        status: "Open",
+        wait_time: "0 Minute",
+        details: "Description goes here"
+    });
+    hikesRef.add({
+        code: "EVENT3",
+        name: "Stanley Park",
+        current_queue: "0",
+        status: "Open",
+        wait_time: "0 Minute",
+        details: "Description goes here"
     });
 }
-sayHello();
 
-function writeWebcamData() {
-    //this is an array of JSON objects copied from open source data
-    var webcams = [{
-            "datasetid": "web-cam-url-links",
-            "recordid": "01d6f80e6ee6e7f801d2b88ad7517bd05223e790",
-            "fields": {
-                "url": "http://images.drivebc.ca/bchighwaycam/pub/html/www/17.html",
-                "geom": {
-                    "type": "Point",
-                    "coordinates": [
-                        -123.136736007805,
-                        49.2972589838826
-                    ]
-                },
-                "mapid": "TCM015",
-                "name": "Stanley Park Causeway"
-            },
-            "record_timestamp": "2021-03-22T10:32:40.391000+00:00"
-        },
-        {
-            "datasetid": "web-cam-url-links",
-            "recordid": "d95ead494c2afbb5f47efdc26bf3ea8c6b8b2e22",
-            "fields": {
-                "url": "http://images.drivebc.ca/bchighwaycam/pub/html/www/20.html",
-                "geom": {
-                    "type": "Point",
-                    "coordinates": [
-                        -123.129968,
-                        49.324891
-                    ]
-                },
-                "mapid": "TCM017",
-                "name": "North End 2"
-            },
-            "record_timestamp": "2021-03-22T10:32:40.391000+00:00"
-        },
-        {
-            "datasetid": "web-cam-url-links",
-            "recordid": "8651b55b799cac55f9b74d654a88f3500b6acd64",
-            "fields": {
-                "url": "https://trafficcams.vancouver.ca/cambie49.htm",
-                "geom": {
-                    "type": "Point",
-                    "coordinates": [
-                        -123.116492357278,
-                        49.2261139995231
-                    ]
-                },
-                "mapid": "TCM024",
-                "name": "Cambie St and W 49th Av",
-                "geo_local_area": "Oakridge"
-            },
-            "record_timestamp": "2021-03-22T10:32:40.391000+00:00"
-        },
-        {
-            "datasetid": "web-cam-url-links",
-            "recordid": "f66fa2c58d19e3f28cf8b842bfa1db073e32e71b",
-            "fields": {
-                "url": "https://trafficcams.vancouver.ca/cambie41.htm",
-                "geom": {
-                    "type": "Point",
-                    "coordinates": [
-                        -123.116192190431,
-                        49.2335434721856
-                    ]
-                },
-                "mapid": "TCM025",
-                "name": "Cambie St and W 41st Av",
-                "geo_local_area": "South Cambie"
-            },
-            "record_timestamp": "2021-03-22T10:32:40.391000+00:00"
-        },
-        {
-            "datasetid": "web-cam-url-links",
-            "recordid": "7c3afe1d3fe4c80f24260a4946abea3fb15b7017",
-            "fields": {
-                "url": "https://trafficcams.vancouver.ca/cambie25.htm",
-                "geom": {
-                    "type": "Point",
-                    "coordinates": [
-                        -123.115406053889,
-                        49.248990875309
-                    ]
-                },
-                "mapid": "TCM026",
-                "name": "Cambie St and W King Edward Av",
-                "geo_local_area": "South Cambie"
-            },
-            "record_timestamp": "2021-03-22T10:32:40.391000+00:00"
-        },
-        {
-            "datasetid": "web-cam-url-links",
-            "recordid": "7fea7df524a205c0c0eb8efcc273345356cbe8d1",
-            "fields": {
-                "url": "https://trafficcams.vancouver.ca/mainTerminal.htm",
-                "geom": {
-                    "type": "Point",
-                    "coordinates": [
-                        -123.100028035364,
-                        49.2727762979223
-                    ]
-                },
-                "mapid": "TCM028",
-                "name": "Main St and Terminal Av",
-                "geo_local_area": "Downtown"
-            },
-            "record_timestamp": "2021-03-22T10:32:40.391000+00:00"
-        }
-    ];
+function displayCards(collection) {
+    let cardTemplate = document.getElementById("eventCardTemplate");
 
-    webcams.forEach(function (cam) { //cycle thru json objects in array
-        console.log(cam); //just to check it out
-        db.collection("webcams").add(cam) //add this new document
-            .then(function (doc) { //success 
-                console.log("wrote to webcams collection " + doc.id);
+    db.collection(collection).get()
+        .then(snap => {
+            var i = 1;
+            snap.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name; // get value of the "name" key
+                var details = doc.data().details; // get value of the "details" key
+                var wait_time = doc.data().wait_time;
+                var status = doc.data().status;
+                let newcard = cardTemplate.content.cloneNode(true);
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-image').src = "./images/" + doc.data().code + ".jpg";
+                newcard.querySelector('.card-info').innerHTML = wait_time + "<br>" + status;
+
+                //give unique ids to all elements for future use
+                newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+                i++;
             })
-    })
+        })
+}
+
+displayCards("events");
+
+async function readJSONwait_times() {
+    const response = await fetch(
+        'https://queue-times.com/parks.json'
+    )
+    const data = await response.text(); //get text file, string
+    const events = JSON.parse(data); //convert to JSON
+    console.log(events);
+    for (x of events) { //iterate thru each event
+        let name = x.name;
+        let status = x.is_open;
+        let wait_time = x.wait_time;
+        let update = x.last_updated;
+        console.log(name);
+        let details = "Name: " + name + "<br>" + "Status: " + status + "<br>" + "Wait Times: " + wait_time + "<br>" + "Last Updated: " + update;
+    }
 }
