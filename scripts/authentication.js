1933bdddc332e5ff24a828d60f672bce18e326ba
Kristopher Ocampo <kris.ocampo@hotmail.com>
2022-03-30 13:41:39 -0700

    Updated function signINSuccessWithAuthResult function to correctly add new users to our Firebase db users collection

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -7,7 +7,26 @@ var uiConfig = {
       // User successfully signed in.
       // Return type determines whether we continue the redirect automatically
       // or whether we leave that to developer to handle.
-      return true;
+      var user = authResult.user; //get the user object info
+      if (authResult.additionalUserInfo.isNewUser) {
+        // create a collection with name "users"
+        db.collection("users")
+          //define a document for a user with UID as a document ID
+          .doc(user.uid).set({
+            name: user.displayName,
+            email: user.email
+          }).then(function () {
+            console.log("New user added to firestore");
+            window.location.assign("main.html");
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+
+      } else {
+        return true;
+      }
+      return false;
     },
     uiShown: function () {
       // The widget is rendered.
@@ -17,11 +36,15 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: './main.html',
+  signInSuccessUrl: 'main.html',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    //firebase.auth.GithubAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //firebase.auth.PhoneAuthProvider.PROVIDER_ID
   ],
   // Terms of service url.
   tosUrl: '<your-tos-url>',
@@ -29,5 +52,4 @@ var uiConfig = {
   privacyPolicyUrl: '<your-privacy-policy-url>'
 };
 
-// The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
