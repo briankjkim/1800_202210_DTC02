64a89ff8facc7d4078c22f961fb448fcc620d8c6
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-03-30 22:06:46 -0700

    implement profile.html

---
+++ scripts/display_page_new.js
@@ -0,0 +1,30 @@
+function populateCardsDynamically() {
+    let eventCardTemplate = document.getElementById("eventCardTemplate");
+    let eventCardGroup = document.getElementById("eventCardGroup");
+    
+    db.collection("events").get()
+        .then(allevents => {
+            allevents.forEach(doc => {
+                var eventName = doc.data().name;
+                var eventCode = doc.data().code;
+                var eventStatus = doc.data().status;
+                var eventWait_time = doc.data().wait_time;
+                var eventCurrent_queue = doc.data().current_queue;
+                var eventDetails = doc.data().details;
+
+
+                let testeventCard = eventCardTemplate.content.cloneNode(true);
+                testeventCard.querySelector('.card-title').innerHTML = eventName;
+                testeventCard.querySelector('.card-code').innerHTML = eventCode;
+                testeventCard.querySelector('.card-status').innerHTML = eventStatus;
+                testeventCard.querySelector('.card-wait_time').innerHTML = eventWait_time;
+                testeventCard.querySelector('.card-current_queue').innerHTML = eventCurrent_queue + "&nbsp people at the moment";
+                testeventCard.querySelector('.card-text').innerHTML = eventDetails;                
+                testeventCard.querySelector('a').onclick = () => setEventData(eventCode);
+                testeventCard.querySelector('img').src = `./images/${eventCode}.jpg`;
+                eventCardGroup.appendChild(testeventCard);
+            })
+
+        })
+}
+populateCardsDynamically();
