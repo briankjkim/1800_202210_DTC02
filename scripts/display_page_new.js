c03f67b19085c16a4f96dad1e62dd2c83c086263
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-04-06 12:24:01 -0700

    fix display

--- scripts/display_page_new.js
+++ scripts/display_page_new.js
@@ -1,5 +1,13 @@
 var currentUser //put this right after you start script tag before writing any functions.
 
+db.collection("events").where("status", "==", "Open")
+    .onSnapshot((querySnapshot) => {
+        var events = [];
+        querySnapshot.forEach((doc) => {
+            events.push(doc.data().name);
+        });
+    });
+
 
 function populateCardsDynamically() {
     let eventCardTemplate = document.getElementById("eventCardTemplate");
@@ -35,59 +43,60 @@ function populateCardsDynamically() {
 populateCardsDynamically();
 
 
-function addQueue(eventCode) {  
+function addQueue(eventCode) {
     console.log("inside");
     db.collection("events").where("code", "==", eventCode)
-    .get()
-    .then(queryevents => {
-        //see how many results you have got from the query
-        size = queryevents.size;
-        // get the documents of query
-        events = queryevents.docs;
-        if (size = 1) {
-            id = events[0].id;
-            console.log(id);
-                                //update method will add to the specified field in database, if that field does not exist, it will create that.
-            db.collection("events").doc(id).update({
-                                        //Firebase documentation has this method for incrementation.
-                current_queue: firebase.firestore.FieldValue.increment(1),
-                wait_time: firebase.firestore.FieldValue.increment(2)
-            })
-            
-        } else {
-            console.log("Query has more than one data")
-        }
-    })
-    .catch((error) => {
-        console.log("Error getting documents: ", error);
-    });
+        .get()
+        .then(queryevents => {
+            //see how many results you have got from the query
+            size = queryevents.size;
+            // get the documents of query
+            events = queryevents.docs;
+            if (size = 1) {
+                id = events[0].id;
+                console.log(id);
+                //update method will add to the specified field in database, if that field does not exist, it will create that.
+                db.collection("events").doc(id).update({
+                    //Firebase documentation has this method for incrementation.
+                    current_queue: firebase.firestore.FieldValue.increment(1),
+                    wait_time: firebase.firestore.FieldValue.increment(2)
+                })
+
+            } else {
+                console.log("Query has more than one data")
+            }
+        })
+        .catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
 }
 
-function removeQueue(eventCode) {  
+function removeQueue(eventCode) {
     console.log("inside");
     db.collection("events").where("code", "==", eventCode)
-    .get()
-    .then(queryevents => {
-        //see how many results you have got from the query
-        size = queryevents.size;
-        // get the documents of query
-        events = queryevents.docs;
-        if (size = 1) {
-            id = events[0].id;
-            console.log(id);
-                                //update method will add to the specified field in database, if that field does not exist, it will create that.
-            db.collection("events").doc(id).update({
-                                        //Firebase documentation has this method for incrementation.
-                current_queue: firebase.firestore.FieldValue.increment(-1),
-                wait_time: firebase.firestore.FieldValue.increment(-2)
-            })
-            
-        } else {
-            console.log("Query has more than one data")
-        }
-    })
-    .catch((error) => {
-        console.log("Error getting documents: ", error);
-    });
+        .get()
+        .then(queryevents => {
+            //see how many results you have got from the query
+            size = queryevents.size;
+            // get the documents of query
+            events = queryevents.docs;
+            if (size = 1) {
+                id = events[0].id;
+                console.log(id);
+                //update method will add to the specified field in database, if that field does not exist, it will create that.
+                db.collection("events").doc(id).update({
+                    //Firebase documentation has this method for incrementation.
+                    current_queue: firebase.firestore.FieldValue.increment(-1),
+                    wait_time: firebase.firestore.FieldValue.increment(-2)
+                })
+
+            } else {
+                console.log("Query has more than one data")
+            }
+        })
+        .catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
 }
 
+// Live listener for changes in status
